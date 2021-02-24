836f292834287afc9b57458d22790aa754413062
Emil Pandocchi <emil@pandocchi.it>
2021-02-24 10:31:10 +0100

    adds routine to add dockerDesktopTemplate

--- libhosty.go
+++ libhosty.go
@@ -492,3 +492,9 @@ func (h *HostsFile) RestoreDefaultDarwinHostsFile() {
 	hfl, _ := ParseHostsFileAsString(darwinHostsTemplate)
 	h.HostsFileLines = hfl
 }
+
+//AddDockerDesktopTemplate adds the dockerDesktopTemplate to the actual hostsFile
+func (h *HostsFile) AddDockerDesktopTemplate() {
+	hfl, _ := ParseHostsFileAsString(dockerDesktopTemplate)
+	h.HostsFileLines = append(h.HostsFileLines, hfl...)
+}
