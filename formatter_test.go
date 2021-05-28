4f901d056f7c62ba707915d38bd2c40c9f5f2381
Emil Pandocchi <emil@pandocchi.it>
2021-05-28 11:20:53 +0200

    improve TestLineFormatter

--- formatter_test.go
+++ formatter_test.go
@@ -1,21 +1,27 @@
 package libhosty
 
 import (
+	"net"
 	"regexp"
 	"testing"
 )
 
-var testHostsFile = `1.1.1.1 my.host.name # This is a host`
-
 func TestLineFormatter(t *testing.T) {
-	// parse fake hosts file
-	hfl, err := ParseHostsFileAsString(testHostsFile)
-	if err != nil {
-		t.Fatalf("unable to parse testHostsFile: %s", err)
+	// define custom hostsFileLine
+	hfl := HostsFileLine{
+		Number:      0,
+		Type:        30,
+		Address:     net.ParseIP("1.1.1.1"),
+		Parts:       []string{""},
+		Hostnames:   []string{"my.host.name"},
+		Raw:         "1.1.1.1 my.host.name # This is a host",
+		Comment:     "This is a host",
+		IsCommented: true,
+		trimed:      "1.1.1.1 my.host.name",
 	}
 
 	// invoke lineFormatter on 1st hosts file line
-	l := lineFormatter(hfl[0])
+	l := lineFormatter(hfl)
 
 	// define what we expect
 	w := regexp.MustCompile("1.1.1.1         \tmy.host.name\t#This is a host")
