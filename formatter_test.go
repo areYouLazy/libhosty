860ed4a35963e03fd6a24f8c8e78b14944a3d404
Emil Pandocchi <emil@pandocchi.it>
2021-05-28 12:14:29 +0200

    improves tests

--- formatter_test.go
+++ formatter_test.go
@@ -2,12 +2,12 @@ package libhosty
 
 import (
 	"net"
-	"regexp"
+	"strings"
 	"testing"
 )
 
 func TestLineFormatter(t *testing.T) {
-	// define custom hostsFileLine
+	// define custom hostsFileLine with IsCommented
 	hfl := HostsFileLine{
 		Number:      0,
 		Type:        30,
@@ -24,10 +24,54 @@ func TestLineFormatter(t *testing.T) {
 	l := lineFormatter(hfl)
 
 	// define what we expect
-	w := regexp.MustCompile("1.1.1.1         \tmy.host.name\t#This is a host")
+	w := "# 1.1.1.1         \tmy.host.name\t#This is a host"
 
 	// check
-	if !w.MatchString(l) {
-		t.Fatalf(`wants '%q' got '%q'`, w, l)
+	if !strings.EqualFold(l, w) {
+		t.Fatalf(`IsCommented=true and Comment: wants '%q' got '%q'`, w, l)
+	}
+
+	// test without IsCommented
+	hfl.IsCommented = false
+	l = lineFormatter(hfl)
+	w = "1.1.1.1         \tmy.host.name\t#This is a host"
+	if !strings.EqualFold(l, w) {
+		t.Fatalf(`IsCommented=false and Comment: wants '%q' got '%q'`, w, l)
+	}
+
+	// test with IsCommented but without comment in line
+	hfl.IsCommented = true
+	hfl.Comment = ""
+	l = lineFormatter(hfl)
+	w = "# 1.1.1.1         \tmy.host.name"
+	if !strings.EqualFold(l, w) {
+		t.Fatalf(`IsCommented=true no Comment: wants '%q' got '%q'`, w, l)
+	}
+
+	// check without IsCommented
+	hfl.IsCommented = false
+	hfl.Comment = ""
+	l = lineFormatter(hfl)
+	w = "1.1.1.1         \tmy.host.name"
+	if !strings.EqualFold(l, w) {
+		t.Fatalf(`IsCommented=false no Comment: wants '%q' got '%q'`, w, l)
+	}
+
+	// define a comment line
+	hfl = HostsFileLine{
+		Number:      0,
+		Type:        20,
+		Address:     []byte{},
+		Parts:       []string{},
+		Hostnames:   []string{},
+		Raw:         "# Comment Line",
+		Comment:     "",
+		IsCommented: false,
+		trimed:      "",
+	}
+	w = "# Comment line"
+	l = lineFormatter(hfl)
+	if !strings.EqualFold(l, w) {
+		t.Fatalf(`Comment: wants '%q' got '%q'`, w, l)
 	}
 }
--- parser_test.go
+++ parser_test.go
@@ -12,6 +12,13 @@ import (
 func TestParseHostsFile(t *testing.T) {
 	var path string
 
+	// check for invalid path
+	path = "/my/custom/invalid/path/hosts"
+	_, err := ParseHostsFile(path)
+	if err == nil {
+		t.Fatalf("should fail with invalid path: %s", path)
+	}
+
 	// define file path
 	switch runtime.GOOS {
 	case "win":
@@ -30,6 +37,7 @@ func TestParseHostsFile(t *testing.T) {
 
 	// check for unknown lines
 	// this can be a false positive if the line is actually invalid
+	// also we assume to have no unknown line in a standard system
 	// but we left it to ensure correct lines are not recognized as unknown lines
 	for k, v := range hf {
 		if v.Type == LineTypeUnknown {
@@ -53,5 +61,27 @@ func TestParseHostsFile(t *testing.T) {
 				t.Fatalf("comment line does not starts with # at index %d: %v", k, v)
 			}
 		}
+	}
+}
+
+//TestParseHostsFileAsString test unknown line type
+// actual parsing is already tested in TestParseHostsFile()
+func TestParseHostsFileAsString(t *testing.T) {
+	// define a custom hosts file with an address line and an invalid line"
+	var fakeHostsFile = `1.1.1.1 my.hosts.file # With Comment
+129dj120isdj12i0`
+
+	hf, err := ParseHostsFileAsString(fakeHostsFile)
+	if err != nil {
+		t.Fatalf("error parsing fakeHostsFile: %s", err)
+	}
+
+	// we expect the 1st line to be of type address
+	if hf[0].Type != LineTypeAddress {
+		t.Fatalf("line should be of type address: %v", hf[0])
+	}
+	// and the 2nd line to be of type unknown
+	if hf[1].Type != LineTypeUnknown {
+		t.Fatalf("line should be of type address: %v", hf[1])
 	}
 }
