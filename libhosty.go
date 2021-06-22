892d1bab81f9798ea93b15ecbae6f8ba8c3732c2
emil@pandocchi.it <emil@pandocchi.it>
2021-06-22 12:28:14 +0200

    trying to fix a bug

--- libhosty.go
+++ libhosty.go
@@ -385,20 +385,12 @@ func (h *HostsFile) RemoveHostsFileLineByHostname(hostname string) {
 func (h *HostsFile) RemoveHostsFileLinesByHostnameAsRegexp(hostname string) {
 	reg := regexp.MustCompile(hostname)
 
-	removed := false
-
 	for idx := len(h.HostsFileLines) - 1; idx >= 0; idx-- {
 		for _, hn := range h.HostsFileLines[idx].Hostnames {
 			if reg.MatchString(hn) {
 				h.RemoveHostsFileLineByRow(idx)
-				removed = true
 				continue
 			}
-		}
-
-		if removed {
-			removed = false
-			continue
 		}
 	}
 }
